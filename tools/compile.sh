2b8e7920da88318b0adfb82b01126c269c407fac
noaione <noaione0809@gmail.com>
2020-11-24 22:53:05 +0700

    tools: add compile tools

---
+++ tools/compile.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Please provide project to compile, example: tools/compile.sh WarungBude"
+    exit 1
+fi
+
+if [ "$1" != "" ]; then
+    if [ "$1" == "WarungBude" ]; then
+        echo "Will compile WarungBude project"
+    else
+        echo "Unknown project"; exit 1
+    fi
+fi
+
+echo "Preparing to compile..."
+if [ "${CXX_TOOLS:-}" != "" ]; then
+    export CXX=${CXX_TOOLS}
+    echo "Using: ${CXX}"
+fi
+
+if [ "${TRAVIS_OS_NAME:-}" == "" ]; then
+	echo "Guessing OS"
+	if uname -s | grep -i Darwin &> /dev/null; then
+		TRAVIS_OS_NAME=osx
+        BUILD_FOR=OSX
+	elif uname -s | grep -i Linux &> /dev/null; then
+		TRAVIS_OS_NAME=linux
+        BUILD_FOR=Linux
+		if [ "${CCX:-}" == "" ]; then
+			# default to g++
+			export CXX=g++
+		fi
+	else
+		echo "Failed to guess OS"; exit 1
+	fi
+fi
+
+if [ "${BUILD_FOR:-}" == "" ]; then
+    BUILD_FOR=unknown
+fi
+
+EXTRA=""
+if [ "${BUILD_FOR:-}" == "Linux" ]; then
+	EXTRA="-Wno-unused-result" # Supress warnings
+fi
+
+mkdir -p build
+echo "OS Detected: ${TRAVIS_OS_NAME}"
+ALL_MODULES=`ls $1/src/modules/*.cpp | tr '\n' ' '`
+OUTPUT_FILE="build/$1_${BUILD_FOR}"
+
+echo "[${TRAVIS_OS_NAME}_${BUILD_FOR}] Building..."
+echo "[${TRAVIS_OS_NAME}_${BUILD_FOR}] Executing: ${CXX} $1/src/main.cpp ${ALL_MODULES} -o ${OUTPUT_FILE} -O2 -s"
+$CXX $1/src/main.cpp $ALL_MODULES -o $OUTPUT_FILE $EXTRA -O2 -s
+echo "[${TRAVIS_OS_NAME}_${BUILD_FOR}] Hashing File for verifcation..."
+sha256sum $OUTPUT_FILE
+echo "[${TRAVIS_OS_NAME}_${BUILD_FOR}] Artifact build!"
